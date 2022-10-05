ae63c7168bda38d1e876738e522a636f6131af7f
VoltG3 <kristians.kodehode@gmail.com>
2022-10-05 18:53:54 +0200

    [ ADDING ] - useWindowDimension()

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+import useWindowDimension from './utils/useWindowDimension'
+
 export default function App() {
+    console.log(useWindowDimension())
+
     return (
         <div>
             <Header />
