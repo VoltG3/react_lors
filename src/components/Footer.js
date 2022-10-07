dfdc1c346e04ccc1e180020a021db4c9f5ec10d6
VoltG3 <kristians.kodehode@gmail.com>
2022-10-07 14:44:34 +0200

    [ UPDATE ] - css header & footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,18 @@
+import styled from 'styled-components'
+
+const StyledFooter = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+      width: 100%;
+      height: 419px;
+`
 
 export default function Footer() {
     return (
-        <>
+        <StyledFooter>
             <p>Footer</p>
-        </>
+        </StyledFooter>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
+import styled from 'styled-components'
+
 import Navigation from './Navigation'
+
+const StyledHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+      width: 100%;
+      height: 419px;
+`
 
 export default function Header() {
     return (
-        <>
+        <StyledHeader>
             <p>Header</p>
             <Navigation />
-        </>
+        </StyledHeader>
     )
 }
