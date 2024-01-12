1dd476e6c2a7342cc065a31b5adb5145cc5eb65b
VoltG3 <kristians.kodehode@gmail.com>
2024-01-12 11:32:57 +0100

    [ UPDATE ] - issues

--- src/GlobalFontStyles.js
+++ src/GlobalFontStyles.js
@@ -5,98 +5,78 @@ export const GlobalFontStyles = createGlobalStyle`
 
       --font--Montserrat: "Montserrat", sans-serif;
       --font--OpenSans: "Open Sans", sans-serif;
-      //--font--CrimsonText: "Crimson Text", serif;
       --font--Roboto: "Roboto", sans-serif;
-
-      //--font--GTWalsheimBold: "GTWalsheimBold", sans-serif;
-      //--font--GTWalsheimMedium: "GTWalsheimMedium", sans-serif;
       --font--GTWalsheimLight: "GTWalsheimLight", sans-serif;
       --font--opacity: 0.7;
-      
-      // intro
-      --textContent--h0--fontsize: min(max(2.5vw, 22px), 36px);
-      --textContent--h0--lineheight: min(max(3.056vw, 24px), 44px);
-      --textContent--h0--rus--fontsize: min(max(2.5vw, 22px), 36px);
-      --textContent--h0--rus--lineheight: min(max(3.056vw, 24px), 44px);
-      
-      --textContent--h1--fontsize: min(max(2.5vw, 18px), 36px);
-      --textContent--h1--lineheight: min(max(3.056vw, 24px), 44px);
-      --textContent--h1--rus--fontsize: min(max(1.944vw, 18px), 28px);
-      --textContent--h1--rus--lineheight: min(max(3.056vw, 24px), 44px);
-      
-      // semiheader
-      --textContent--h2--fontsize: min(max(1.389vw, 18px), 20px);                                    
-      --textContent--h2--lineheight: min(max(3.056vw, 22px), 44px);
-      --textContent--h2--rus--fontsize: min(max(1.389vw, 18px), 20px);
-      --textContent--h2--rus--lineheight: min(max(3.056vw, 22px), 44px);
-      
-      // h3 to delete
-      --textContent--h3--fontsize: 20px;
-      
-      --textContent--paragraph--fontsize: min(max(1.458vw, 18px), 21px);
-      --textContent--paragraph--lineheight: min(max(1.806vw, 20px), 26px);
-      --textContent--paragraph--rus--fontsize: min(max(1.458vw, 18px), 21px);
-      --textContent--paragraph--rus--lineheight: min(max(1.806vw, 20px), 26px);
-      // lists
-      --textContent--list--header--fontsize: min(max(1.389vw, 16px), 20px);
-      --textContent--list--item--fontsize: min(max(1.250vw, 14px), 18px);
-      // header
-      --textContent--header--navigation--languages--fontsize: min(max(0.972vw, 10px), 16px);
-      --textContent--header--navigation--infoline--fontsize: min(max(0.972vw, 10px), 14px);
-      --textContent--header--desktop--navigation--fontsize: min(max(0.972vw, 10px), 16px);
-      // footer
-      --textContent--footer--navigation--fontsize: 14px;
-      --textContent--footer--navigation--lineheight: 19px;
-      --textContent--footer--header--fontsize: 14px;
-      --textContent--footer--header--lineheight: 20px;
-      --textContent--footer--item--fontsize: 14px;
-      --textContent--footer--item--lineheight: 20px;
-      // tabledata
-      --textContent--table--header--fontsize: 28px;
-      --textContent--table--header--rus--fontsie: 20px;
-      --textContent--table--item--fontsize: 18px;
-      --textContent--table--item--lineheight: 20px;
-      --textContent--table--item--rus--fontsize: 16px;
-      --textContent--table--item--rus--lineheight: 20px;
-      
-      
-      
-      
-      // [ BUTTONS ] -----------------------------------------------
+        
+    // [ BUTTON ] ------------------------------------------------
 
-      .textContent--button {
-        font-family: var(--font--Roboto),sans-serif;
+    .textContent--button {
+        font-family: var(--font--Roboto), sans-serif;
         font-style: normal;
         font-weight: 400;
         font-size: 15px;
         line-height: 1.5;
-        //opacity: var(--font--opacity);
         color: var(--color--button--v1);
-      }
-      
-          .textContent--button--contacts {
-            font-family: var(--font--Roboto),sans-serif;
-            font-style: normal;
-            font-weight: 400;
-            font-size: 16px;
-            line-height: 1.5;
-            //opacity: var(--font--opacity);
-            color: var(--color--button--v1);
-          }
-    
-          .textContent--button--appointment {
-            font-family: var(--font--Roboto),sans-serif;
-            font-size: 15px;
-            font-weight: 400;
-            line-height: 1.5;
-            // text-align: start;
-            color: var(--color--button--v1);
-            //border: solid 1px red;
-          }
-      
-      // [ COMMON ] ------------------------------------------------
-      // [ NEW ] ---------------------------------------------------
-      .textContent--h0 {
+    }
+        
+    // [ HEADER ] ------------------------------------------------
+        
+    --textContent--header--navigation--languages--fontsize: min(max(0.972vw, 10px), 16px);
+    --textContent--header--navigation--infoline--fontsize: min(max(0.972vw, 10px), 14px);
+    --textContent--header--desktop--navigation--fontsize: min(max(0.972vw, 10px), 16px);  
+        
+    .textContent--header--navigation--languages {
+        font-family: var(--font--OpenSans),sans-serif;
+        font-style: normal;
+        font-weight: 300;
+        font-size: var(--textContent--header--navigation--languages--fontsize);
+        color: var(--color--default);
+    }
+
+    .textContent--header--desktop--infoline {
+        font-family: var(--font--OpenSans),sans-serif;
+        font-style: normal;
+        font-weight: 300;
+        font-size: var(--textContent--header--navigation--infoline--fontsize);
+        color: var(--color--default);
+    }
+
+    .textContent--header--desktop--navigation {
+        font-family: var(--font--OpenSans),sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: var(--textContent--header--desktop--navigation--fontsize);
+        text-transform: uppercase;
+        opacity: var(--font--opacity);
+        color: var(--clr--primary--base);
+    }
+
+    .textContent--header--mobile--navigation {
+        font-family: var(--font--OpenSans),sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 21px;
+        line-height: 33px;
+        text-transform: uppercase;
+    }
+
+    .textContent--header--mobile--languages {
+        font-family: var(--font--OpenSans),sans-serif;
+        font-style: normal;
+        font-weight: 300;
+        font-size: 23px;
+        text-transform: uppercase;
+    }
+        
+    // [ SLIDER ] ------------------------------------------------
+        
+    --textContent--h0--fontsize: min(max(2.5vw, 22px), 36px);
+    --textContent--h0--lineheight: min(max(3.056vw, 24px), 44px);
+    --textContent--h0--rus--fontsize: min(max(2.5vw, 22px), 36px);
+    --textContent--h0--rus--lineheight: min(max(3.056vw, 24px), 44px);    
+        
+    .textContent--h0 {
         font-family: var(--font--GTWalsheimMedium),sans-serif;
         font-style: normal;
         font-weight: 400;
@@ -105,11 +85,9 @@ export const GlobalFontStyles = createGlobalStyle`
         white-space: pre-wrap;
         opacity: var(--font--opacity);
         color: var(--color--secondary);
-        
-       // margin-bottom: .5em;
-      }
+    }
 
-      .textContent--h0--rus {
+    .textContent--h0--rus {
         font-family: var(--font--Montserrat),sans-serif;
         font-style: normal;
         font-weight: 400;
@@ -118,31 +96,47 @@ export const GlobalFontStyles = createGlobalStyle`
         text-transform: uppercase;
         opacity: var(--font--opacity);
         color: var(--color--secondary);
-      }
-      // [ NEW ] ---------------------------------------------------
-      .textContent--h1 {
+    }
+
+    // [ TEXTCONTENT ] -------------------------------------------
+
+   --textContent--h1--fontsize: min(max(2.5vw, 18px), 36px);
+   --textContent--h1--lineheight: min(max(3.056vw, 24px), 44px);
+   --textContent--h1--rus--fontsize: min(max(1.944vw, 18px), 28px);
+   --textContent--h1--rus--lineheight: min(max(3.056vw, 24px), 44px);
+   --textContent--h2--fontsize: min(max(1.389vw, 18px), 20px);
+   --textContent--h2--lineheight: min(max(3.056vw, 22px), 44px);
+   --textContent--h2--rus--fontsize: min(max(1.389vw, 18px), 20px);
+   --textContent--h2--rus--lineheight: min(max(3.056vw, 22px), 44px);
+   --textContent--h3--fontsize: 20px;
+   --textContent--paragraph--fontsize: min(max(1.458vw, 18px), 21px);
+   --textContent--paragraph--lineheight: min(max(1.806vw, 20px), 26px);
+   --textContent--paragraph--rus--fontsize: min(max(1.458vw, 18px), 21px);
+   --textContent--paragraph--rus--lineheight: min(max(1.806vw, 20px), 26px);
+        
+    .textContent--h1 {
         font-family: var(--font--GTWalsheimMedium),sans-serif;
         font-style: normal;
         font-weight: 400;
         font-size: var(--textContent--h1--fontsize);
         line-height: var(--textContent--h1--lineheight)!important;
-          white-space: pre-wrap;
+        white-space: pre-wrap;
         opacity: var(--font--opacity);
         color: var(--color--secondary);
-      }
+    }
 
-      .textContent--h1--rus {
+    .textContent--h1--rus {
         font-family: var(--font--Montserrat),sans-serif;
         font-style: normal;
         font-weight: 400;
         font-size: var(--textContent--h1--rus--fontsize);
         line-height: var(--textContent--h1--rus--lineheight);!important;
-          text-transform: uppercase;
+        text-transform: uppercase;
         opacity: var(--font--opacity);
         color: var(--color--secondary);
-      }
+    }
 
-      .textContent--paragraph {
+    .textContent--paragraph {
         font-family: var(--font--GTWalsheimLight),sans-serif;
         font-style: normal;
         font-weight: 400;
@@ -150,9 +144,9 @@ export const GlobalFontStyles = createGlobalStyle`
         line-height: var(--textContent--paragraph--lineheight);
         opacity: var(--font--opacity);
         color: var(--color--default);
-      }
+    }
 
-      .textContent--paragraph--rus {
+    .textContent--paragraph--rus {
         font-family: var(--font--Montserrat),sans-serif;
         font-style: normal;
         font-weight: 400;
@@ -160,9 +154,9 @@ export const GlobalFontStyles = createGlobalStyle`
         line-height: var(--textContent--paragraph--rus--lineheight);
         opacity: var(--font--opacity);
         color: var(--color--default);
-      }
+    }
 
-      .textContent--h2 {
+    .textContent--h2 {
         font-family: var( --font--GTWalsheimMedium),sans-serif;
         font-style: normal;
         font-weight: 400;
@@ -171,9 +165,9 @@ export const GlobalFontStyles = createGlobalStyle`
         text-transform: uppercase;
         opacity: var(--font--opacity);
         color: var(--color--secondary);
-      }
+    }
 
-      .textContent--h2--rus {
+    .textContent--h2--rus {
         font-family: var(--font--Montserrat),sans-serif;
         font-style: normal;
         font-weight: 400;
@@ -182,20 +176,60 @@ export const GlobalFontStyles = createGlobalStyle`
         text-transform: uppercase;
         opacity: var(--font--opacity);
         color: var(--color--secondary);
-      }
+    }
 
-      .textContent--h3 {
+    .textContent--h3 {
         font-family: var(--font--OpenSans),sans-serif;
         font-style: normal;
         font-weight: 600;
         font-size: var(--textContent--h3--fontsize);
         opacity: var(--font--opacity);
-        color: var(--color--primary);
-      }
+    }
+        
+    // [ TABLEDATA ] ---------------------------------------------
 
-      // [ LISTS ] -------------------------------------------------
+    .textContent--tabledata--header {
+        font-family: var(--font--Roboto),sans-serif;
+        font-size: 26px;
+        font-weight: 300;
+        color: var(--color--tabledata);
 
-      .textContent--list--header {
+        @media only screen and (max-width: 700px) {
+            font-size: 20px;
+        }
+    }
+
+    .textContent--tabledata--item--small {
+        font-family: var(--font--Roboto),sans-serif;
+        font-size: 14px;
+        font-weight: 300;
+        line-height: 1.5;
+        color: var(--color--button--v1);
+
+        @media only screen and (max-width: 700px) {  
+            line-height: 1.3;
+            font-size: 14px;
+        }
+    }
+
+    .textContent--tabledata--item--names {
+        font-family: var(--font--OpenSans),sans-serif;
+        font-size: 12px;
+        font-weight: 600;
+        opacity: var(--font--opacity);
+        color: var(--color--button--v1);
+
+        @media only screen and (max-width: 700px) {
+            font-size: 10px;
+        }
+    }   
+        
+    // [ LIST ] --------------------------------------------------
+
+    --textContent--list--header--fontsize: min(max(1.389vw, 16px), 20px);
+    --textContent--list--item--fontsize: min(max(1.250vw, 14px), 18px);
+        
+    .textContent--list--header {
         font-family: var(--font--Montserrat),sans-serif;
         font-style: normal;
         font-weight: 600;
@@ -204,72 +238,29 @@ export const GlobalFontStyles = createGlobalStyle`
         color: var(--color--none);
 
         @media only screen and (max-width: 700px) {
-          font-weight: 400;
+            font-weight: 400;
         }
-      }
+    }
 
-      .textContent--list--item {
+    .textContent--list--item {
         font-family: var(--font--Montserrat),sans-serif;
         font-style: normal;
         font-weight: 400;
         font-size: var(--textContent--list--item--fontsize);
         opacity: var(--font--opacity);
         color: var(--color--secondary);
-      }
+    }  
 
-      // [ HEADER ] ------------------------------------------------
-
-      .textContent--header--navigation--languages {
-        font-family: var(--font--OpenSans),sans-serif;
-        font-style: normal;
-        font-weight: 300;
-        font-size: var(--textContent--header--navigation--languages--fontsize);
-        color: var(--color--default);
-      }
-
-      .textContent--header--desktop--infoline {
-        font-family: var(--font--OpenSans),sans-serif;
-        font-style: normal;
-        font-weight: 300;
-        font-size: var(--textContent--header--navigation--infoline--fontsize);
-        color: var(--color--default);
-      }
-
-      .textContent--header--desktop--navigation {
-        font-family: var(--font--OpenSans),sans-serif;
-        font-style: normal;
-        font-weight: 400;
-        font-size: var(--textContent--header--desktop--navigation--fontsize);
-        text-transform: uppercase;
-        opacity: var(--font--opacity);
-        color: var(--clr--primary--base);
-      }
-
-      .textContent--header--mobile--navigation {
-        font-family: var(--font--OpenSans),sans-serif;
-        font-style: normal;
-        font-weight: 400;
-        font-size: 21px;
-        line-height: 33px;
-        text-transform: uppercase;
-        //opacity: var(--font--opacity);
-        //color: var(--color--none);
-      }
-
-      .textContent--header--mobile--languages {
-        font-family: var(--font--OpenSans),sans-serif;
-        font-style: normal;
-        font-weight: 300;
-        font-size: 23px;
-        text-transform: uppercase;
-        //opacity: var(--font--opacity);
-        //color: var(--color--none);
-
-      }
-
-      // [ FOOTER ] ------------------------------------------------
-
-      .textContent--footer--navigation {
+    // [ FOOTER ] ------------------------------------------------
+        
+    --textContent--footer--navigation--fontsize: 14px;
+    --textContent--footer--navigation--lineheight: 19px;
+    --textContent--footer--header--fontsize: 14px;
+    --textContent--footer--header--lineheight: 20px;
+    --textContent--footer--item--fontsize: 14px;
+    --textContent--footer--item--lineheight: 20px;
+        
+    .textContent--footer--navigation {
         font-family: var(--font--OpenSans),sans-serif;
         font-style: normal;
         font-weight: 600;
@@ -279,118 +270,26 @@ export const GlobalFontStyles = createGlobalStyle`
         color: var(--color--paragraph--footer);
 
         & :hover {
-          color: var(--color--none);
+            color: var(--color--none);
         }
-      }
-      
-      .textContent--footer--header {
+    }
+               
+    .textContent--footer--head {
         font-family: var(--font--OpenSans),sans-serif;
         font-style: normal;
         font-weight: 600;
         font-size: var(--textContent--footer--header--fontsize);
         line-height: var(--textContent--footer--header--lineheight);
         color: var(--color--none);
-      }
+    }
 
-      .textContent--footer--item {
+    .textContent--footer--item {
         font-family: var(--font--OpenSans),sans-serif;
         font-style: normal;
         font-weight: 600;
         font-size: var(--textContent--footer--item--fontsize);
         line-height: var(--textContent--footer--item--lineheight);
         color: var(--color--paragraph--footer);
-      }
-
-      // [ TABLEDATA ] ---------------------------------------------
-      // [ NEW ] ---------------------------------------------------
-
-      .textContent--tabledata--header {
-        font-family: var(--font--Roboto),sans-serif;
-        font-size: 26px;
-        font-weight: 300;
-        color: var(--color--tabledata);
-
-        @media only screen and (max-width: 700px) {
-          font-size: 20px;
-        }
-      }
-
-      .textContent--tabledata--item {
-        font-family: var(--font--Roboto),sans-serif;
-        font-size: 18px;
-        font-weight: 300;
-        line-height: 1.5;
-        color: var(--color--button--v1);
-
-        @media only screen and (max-width: 700px) {
-          line-height: 1.3;
-          font-size: 16px;
-        }
-      }
-
-      .textContent--tabledata--item--small {
-        font-family: var(--font--Roboto),sans-serif;
-        font-size: 14px;
-        font-weight: 300;
-        line-height: 1.5;
-        color: var(--color--button--v1);
-        
-        @media only screen and (max-width: 700px) {
-          line-height: 1.3;
-          font-size: 14px;
-        }
-      }
-
-      .textContent--tabledata--item--names {
-        font-family: var(--font--OpenSans),sans-serif;
-        font-size: 12px;
-        font-weight: 600;
-        opacity: var(--font--opacity);
-        color: var(--color--button--v1);
-      
-        @media only screen and (max-width: 700px) {
-          font-size: 10px;
-        }
-      }
-
-      // [ NEW ] ---------------------------------------------------
-      
-      .textContent--table--header {
-        font-family: var(--font--GTWalsheimMedium),sans-serif;
-        font-style: normal;
-        font-weight: 600;
-        font-size: var(--textContent--table--header--fontsize);
-        opacity: var(--font--opacity);
-        color: var(--color--primary);
-      }
-
-      .textContent--table--header--rus {
-        font-family: var(--font--Montserrat),sans-serif;
-        font-style: normal;
-        font-weight: 600;
-        font-size: var(--textContent--table--header--rus--fontsize);
-        opacity: var(--font--opacity);
-        color: var(--color--primary);
-      }
-
-      .textContent--table--item {
-        font-family: var(--font--GTWalsheimLight),sans-serif;
-        font-style: normal;
-        font-weight: 400;
-        font-size: var(--textContent--table--item--fontsize);
-        line-height: var(--textContent--table--item--lineheight);
-        opacity: var(--font--opacity);
-        color: var(--color--primary);
-      }
-
-      .textContent--table--item--rus {
-        font-family: var(--font--Montserrat),sans-serif;
-        font-style: normal;
-        font-weight: 400;
-        font-size: var(--textContent--table--item--rus--fontsize);
-        line-height: var(--textContent--table--item--rus--lineheight);
-        opacity: var(--font--opacity);
-        color: var(--color--primary);
-      }
     }
+  }
 `
