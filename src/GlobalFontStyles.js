d49a116f79d3ce8cd4b31d7822e01430981f6ed9
VoltG3 <kristiandanoss@gmail.com>
2026-01-11 03:34:12 +0100

    [ UPDATE ] - issues

--- src/GlobalFontStyles.js
+++ src/GlobalFontStyles.js
@@ -1,17 +1,9 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalFontStyles = createGlobalStyle`
-    :root {
-
-      --font--Montserrat: "Montserrat", sans-serif;
-      --font--OpenSans: "Open Sans", sans-serif;
-      --font--Roboto: "Roboto", sans-serif;
-      --font--opacity: 0.7;
-        
-    // [ BUTTON ] ------------------------------------------------
-
+    /* [ BUTTON ] */
     .textContent--button {
-        font-family: var(--font--Roboto), sans-serif;
+        font-family: "Roboto", sans-serif;
         font-style: normal;
         font-weight: 400;
         font-size: 15px;
@@ -19,177 +11,106 @@ export const GlobalFontStyles = createGlobalStyle`
         color: #262626;
     }
         
-    // [ HEADER ] ------------------------------------------------
-        
-    --textContent--header--navigation--languages--fontsize: min(max(0.972vw, 10px), 16px);
-    --textContent--header--navigation--infoline--fontsize: min(max(0.972vw, 10px), 14px);
-    --textContent--header--desktop--navigation--fontsize: min(max(0.972vw, 10px), 16px);  
-        
-    .textContent--header--navigation--languages {
-        font-family: var(--font--OpenSans),sans-serif;
-        font-style: normal;
-        font-weight: 300;
-        font-size: var(--textContent--header--navigation--languages--fontsize);
-        color: #030303;
-    }
-
-    .textContent--header--desktop--infoline {
-        font-family: var(--font--OpenSans),sans-serif;
-        font-style: normal;
-        font-weight: 300;
-        font-size: var(--textContent--header--navigation--infoline--fontsize);
-        color: #030303;
-    }
-
-    .textContent--header--desktop--navigation {
-        font-family: var(--font--OpenSans),sans-serif;
-        font-style: normal;
-        font-weight: 400;
-        font-size: var(--textContent--header--desktop--navigation--fontsize);
-        text-transform: uppercase;
-        opacity: var(--font--opacity);
-        color: #132743;
-    }
-
-    .textContent--header--mobile--navigation {
-        font-family: var(--font--OpenSans),sans-serif;
-        font-style: normal;
-        font-weight: 400;
-        font-size: 21px;
-        line-height: 33px;
-        text-transform: uppercase;
-    }
-
-    .textContent--header--mobile--languages {
-        font-family: var(--font--OpenSans),sans-serif;
-        font-style: normal;
-        font-weight: 300;
-        font-size: 23px;
-        text-transform: uppercase;
-    }
-        
-    // [ SLIDER ] ------------------------------------------------
-        
-    --textContent--h0--fontsize: min(max(2.5vw, 22px), 36px);
-    --textContent--h0--lineheight: min(max(3.056vw, 24px), 44px);
-    --textContent--h0--rus--fontsize: min(max(2.5vw, 22px), 36px);
-    --textContent--h0--rus--lineheight: min(max(3.056vw, 24px), 44px);    
-        
+    /* [ SLIDER ] */
     .textContent--h0 {
-        font-family: var(--font--Montserrat),sans-serif;
+        font-family: "Montserrat", sans-serif;
         font-style: normal;
         font-weight: 500;
-        font-size: var(--textContent--h0--fontsize);
-        line-height: var(--textContent--h0--lineheight) !important;
+        font-size: min(max(2.5vw, 22px), 36px);
+        line-height: min(max(3.056vw, 24px), 44px) !important;
         white-space: pre-wrap;
-        opacity: var(--font--opacity);
+        opacity: 0.7;
         color: #152F3F;
     }
 
     .textContent--h0--rus {
-        font-family: var(--font--Montserrat),sans-serif;
+        font-family: "Montserrat", sans-serif;
         font-style: normal;
         font-weight: 400;
-        font-size: var(--textContent--h0--rus--fontsize);
-        line-height: var(--textContent--h0--rus--lineheight) !important;
+        font-size: min(max(2.5vw, 22px), 36px);
+        line-height: min(max(3.056vw, 24px), 44px) !important;
         text-transform: uppercase;
-        opacity: var(--font--opacity);
+        opacity: 0.7;
         color: #152F3F;
     }
 
-    // [ TEXTCONTENT ] -------------------------------------------
-
-   --textContent--h1--fontsize: min(max(2.5vw, 20px), 36px);
-   --textContent--h1--lineheight: 1.4;
-   --textContent--h1--rus--fontsize: min(max(2.5vw, 20px), 36px);
-   --textContent--h1--rus--lineheight: 1.4;
-   --textContent--h2--fontsize: min(max(1.8vw, 18px), 24px);
-   --textContent--h2--lineheight: 1.5;
-   --textContent--h2--rus--fontsize: min(max(1.8vw, 18px), 24px);
-   --textContent--h2--rus--lineheight: 1.5;
-   --textContent--h3--fontsize: 20px;
-   --textContent--paragraph--fontsize: min(max(1.2vw, 16px), 18px);
-   --textContent--paragraph--lineheight: 1.6;
-   --textContent--paragraph--rus--fontsize: min(max(1.2vw, 16px), 18px);
-   --textContent--paragraph--rus--lineheight: 1.6;
-        
+    /* [ TEXTCONTENT ] */
     .textContent--h1 {
-        font-family: var(--font--Montserrat),sans-serif;
+        font-family: "Montserrat", sans-serif;
         font-style: normal;
         font-weight: 500;
-        font-size: var(--textContent--h1--fontsize);
-        line-height: var(--textContent--h1--lineheight) !important;
+        font-size: min(max(2.5vw, 20px), 36px);
+        line-height: 1.4 !important;
         white-space: pre-wrap;
-        opacity: var(--font--opacity);
+        opacity: 0.7;
         color: #152F3F;
     }
 
     .textContent--h1--rus {
-        font-family: var(--font--Montserrat),sans-serif;
+        font-family: "Montserrat", sans-serif;
         font-style: normal;
         font-weight: 400;
-        font-size: var(--textContent--h1--rus--fontsize);
-        line-height: var(--textContent--h1--rus--lineheight) !important;
+        font-size: min(max(2.5vw, 20px), 36px);
+        line-height: 1.4 !important;
         text-transform: uppercase;
-        opacity: var(--font--opacity);
+        opacity: 0.7;
         color: #152F3F;
     }
 
     .textContent--paragraph {
-        font-family: var(--font--Montserrat),sans-serif;
+        font-family: "Montserrat", sans-serif;
         font-style: normal;
         font-weight: 300;
-        font-size: var(--textContent--paragraph--fontsize);
-        line-height: var(--textContent--paragraph--lineheight);
-        opacity: var(--font--opacity);
+        font-size: min(max(1.2vw, 16px), 18px);
+        line-height: 1.6;
+        opacity: 0.7;
         color: #030303;
     }
 
     .textContent--paragraph--rus {
-        font-family: var(--font--Montserrat),sans-serif;
+        font-family: "Montserrat", sans-serif;
         font-style: normal;
         font-weight: 400;
-        font-size: var(--textContent--paragraph--rus--fontsize);
-        line-height: var(--textContent--paragraph--rus--lineheight);
-        opacity: var(--font--opacity);
+        font-size: min(max(1.2vw, 16px), 18px);
+        line-height: 1.6;
+        opacity: 0.7;
         color: #030303;
     }
 
     .textContent--h2 {
-        font-family: var( --font--Montserrat),sans-serif;
+        font-family: "Montserrat", sans-serif;
         font-style: normal;
         font-weight: 500;
-        font-size: var(--textContent--h2--fontsize);
-        line-height: var(--textContent--h2--lineheight);
+        font-size: min(max(1.8vw, 18px), 24px);
+        line-height: 1.5;
         text-transform: uppercase;
-        opacity: var(--font--opacity);
+        opacity: 0.7;
         color: #152F3F;
     }
 
     .textContent--h2--rus {
-        font-family: var(--font--Montserrat),sans-serif;
+        font-family: "Montserrat", sans-serif;
         font-style: normal;
         font-weight: 400;
-        font-size: var(--textContent--h2--rus--fontsize);
-        line-height: var(--textContent--h2--rus--lineheight);
+        font-size: min(max(1.8vw, 18px), 24px);
+        line-height: 1.5;
         text-transform: uppercase;
-        opacity: var(--font--opacity);
+        opacity: 0.7;
         color: #152F3F;
     }
 
     .textContent--h3 {
-        font-family: var(--font--OpenSans),sans-serif;
+        font-family: "Open Sans", sans-serif;
         font-style: normal;
         font-weight: 600;
-        font-size: var(--textContent--h3--fontsize);
+        font-size: 20px;
         line-height: 1.5;
-        opacity: var(--font--opacity);
+        opacity: 0.7;
     }
         
-    // [ TABLEDATA ] ---------------------------------------------
-
+    /* [ TABLEDATA ] */
     .textContent--tabledata--header {
-        font-family: var(--font--Montserrat),sans-serif;
+        font-family: "Montserrat", sans-serif;
         font-size: 22px;
         font-weight: 500;
         line-height: 1.4;
@@ -202,7 +123,7 @@ export const GlobalFontStyles = createGlobalStyle`
     }
 
     .textContent--tabledata--item--small {
-        font-family: var(--font--Montserrat),sans-serif;
+        font-family: "Montserrat", sans-serif;
         font-size: 15px;
         font-weight: 300;
         line-height: 1.6;
@@ -213,29 +134,12 @@ export const GlobalFontStyles = createGlobalStyle`
         }
     }
 
-    .textContent--tabledata--item--names {
-        font-family: var(--font--OpenSans),sans-serif;
-        font-size: 12px;
-        font-weight: 600;
-        opacity: var(--font--opacity);
-        color: #262626;
-
-        @media only screen and (max-width: 700px) {
-            font-size: 10px;
-        }
-    }   
-        
-    // [ LIST ] --------------------------------------------------
-
-    --textContent--list--header--fontsize: min(max(1.389vw, 16px), 20px);
-    --textContent--list--item--fontsize: min(max(1.250vw, 14px), 16px);
-    --textContent--list--item--lineheight: 1.6;
-        
+    /* [ LIST ] */
     .textContent--list--header {
-        font-family: var(--font--Montserrat),sans-serif;
+        font-family: "Montserrat", sans-serif;
         font-style: normal;
         font-weight: 600;
-        font-size: var(--textContent--list--header--fontsize);
+        font-size: min(max(1.389vw, 16px), 20px);
         line-height: 1.4;
         text-transform: uppercase;
         color: #FFFFFF;
@@ -247,54 +151,31 @@ export const GlobalFontStyles = createGlobalStyle`
     }
 
     .textContent--list--item {
-        font-family: var(--font--Montserrat),sans-serif;
+        font-family: "Montserrat", sans-serif;
         font-style: normal;
         font-weight: 400;
-        font-size: var(--textContent--list--item--fontsize);
-        line-height: var(--textContent--list--item--lineheight);
-        opacity: var(--font--opacity);
+        font-size: min(max(1.250vw, 14px), 16px);
+        line-height: 1.6;
+        opacity: 0.7;
         color: #152F3F;
     }  
 
-    // [ FOOTER ] ------------------------------------------------
-        
-    --textContent--footer--navigation--fontsize: 14px;
-    --textContent--footer--navigation--lineheight: 19px;
-    --textContent--footer--header--fontsize: 14px;
-    --textContent--footer--header--lineheight: 20px;
-    --textContent--footer--item--fontsize: 14px;
-    --textContent--footer--item--lineheight: 20px;
-        
-    .textContent--footer--navigation {
-        font-family: var(--font--OpenSans),sans-serif;
-        font-style: normal;
-        font-weight: 600;
-        font-size: var(--textContent--footer--navigation--fontsize);
-        line-height: var(--textContent--footer--navigation--lineheight);
-        text-transform: uppercase;
-        color: #828181;
-
-        &:hover {
-            color: #FFFFFF;
-        }
-    }
-               
+    /* [ FOOTER ] */
     .textContent--footer--head {
-        font-family: var(--font--OpenSans),sans-serif;
+        font-family: "Open Sans", sans-serif;
         font-style: normal;
         font-weight: 600;
-        font-size: var(--textContent--footer--header--fontsize);
-        line-height: var(--textContent--footer--header--lineheight);
+        font-size: 14px;
+        line-height: 20px;
         color: #FFFFFF;
     }
 
     .textContent--footer--item {
-        font-family: var(--font--OpenSans),sans-serif;
+        font-family: "Open Sans", sans-serif;
         font-style: normal;
         font-weight: 600;
-        font-size: var(--textContent--footer--item--fontsize);
-        line-height: var(--textContent--footer--item--lineheight);
+        font-size: 14px;
+        line-height: 20px;
         color: #828181;
     }
-  }
 `
